8d9108d8b2b336ee803e22b1174a3d2554f8ee00
Steven Fackler <sfackler@gmail.com>
2015-12-12 20:37:17 -0800

    Finish day 1

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_bytes!("input");
+
+    let mut floor = 0;
+    for &byte in &input[..] {
+        match byte {
+            b'(' => floor += 1,
+            b')' => floor -= 1,
+            _ => {}
+        }
+    }
+
+    println!("{}", floor);
 }
