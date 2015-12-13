3a16640a11c4959aa0438c0beb3ea4050f49145f
Steven Fackler <sfackler@gmail.com>
2015-12-12 20:55:22 -0800

    Finish day 1 part 2

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,14 +1,22 @@
+use std::ops::Add;
+
 fn main() {
     let input = include_bytes!("input");
 
-    let mut floor = 0;
-    for &byte in &input[..] {
-        match byte {
-            b'(' => floor += 1,
-            b')' => floor -= 1,
-            _ => {}
-        }
-    }
+    let it = || {
+        input.iter()
+             .map(|&b| {
+                 match b {
+                     b'(' => 1,
+                     b')' => -1,
+                     _ => 0
+                 }
+             })
+    };
 
+    let floor = it().fold(0, Add::add);
     println!("{}", floor);
+
+    let index = it().scan(0, |a, b| { *a = *a + b; Some(*a) }).position(|x| x == -1).unwrap() + 1;
+    println!("{}", index);
 }
