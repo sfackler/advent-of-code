5967989546ae654252793609a3e259faabae0298
Steven Fackler <sfackler@gmail.com>
2015-12-12 21:22:57 -0800

    Day 3 part 2

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -9,15 +9,37 @@ fn main() {
     locations.insert((0, 0));
 
     for direction in input.chars() {
-        match direction {
-            '^' => location.1 += 1,
-            '>' => location.0 += 1,
-            'v' => location.1 -= 1,
-            '<' => location.0 -= 1,
-            _ => {}
-        }
+        location = shift(location, direction);
         locations.insert(location);
     }
 
     println!("{}", locations.len());
+
+    locations.clear();
+    locations.insert((0, 0));
+    let mut location_a = (0, 0);
+    let mut location_b = (0, 0);
+    for (i, direction) in input.chars().enumerate() {
+        let location = if i % 2 == 0 {
+            &mut location_a
+        } else {
+            &mut location_b
+        };
+
+        *location = shift(*location, direction);
+        locations.insert(*location);
+    }
+
+    println!("{}", locations.len());
+}
+
+fn shift(mut location: (i32, i32), direction: char) -> (i32, i32) {
+    match direction {
+        '^' => location.1 += 1,
+        '>' => location.0 += 1,
+        'v' => location.1 -= 1,
+        '<' => location.0 -= 1,
+        _ => {}
+    }
+    location
 }
