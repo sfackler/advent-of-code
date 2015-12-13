448181f64810f3b7d6b96d0012d93bd5ad8bd1a2
Steven Fackler <sfackler@gmail.com>
2015-12-12 21:38:29 -0800

    Day 4 part 2

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -8,12 +8,23 @@ use std::io::Write;
 fn main() {
     let base = "bgvyzdsv";
     let mut hasher = Hasher::new(Type::MD5);
-    for i in 0.. {
+
+    let mut zeroes = |i| {
         hasher.write_all(format!("{}{}", base, i).as_bytes()).unwrap();
         let hash = hasher.finish().to_hex();
 
-        let count = hash.chars().take_while(|&x| x == '0').count();
-        if count >= 5 {
+        hash.chars().take_while(|&x| x == '0').count()
+    };
+
+    for i in 0.. {
+        if zeroes(i) >= 5 {
+            println!("{}", i);
+            break;
+        }
+    }
+
+    for i in 0.. {
+        if zeroes(i) >= 6 {
             println!("{}", i);
             break;
         }
