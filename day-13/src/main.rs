8690c7720dae5a0a28de077f9193803e7dad9ef9
Steven Fackler <sfackler@gmail.com>
2015-12-13 21:46:13 -0800

    Day 13 part 2

--- day-13/src/main.rs
+++ day-13/src/main.rs
@@ -27,8 +27,21 @@ fn main() {
     }
 
     let mut people = people.into_iter().collect::<Vec<_>>();
-    let mut people = Heap::new(&mut people);
+    println!("{}", max_arrangement(&mut people, &feels));
 
+    for &person in &people {
+        feels.insert(("", person), 0);
+        feels.insert((person, ""), 0);
+    }
+    people.push("");
+
+    println!("{}", max_arrangement(&mut people, &feels));
+}
+
+fn max_arrangement(people: &mut [&'static str],
+                   feels: &HashMap<(&'static str, &'static str), i32>)
+                   -> i32 {
+    let mut people = Heap::new(people);
     let mut max = i32::min_value();
     while let Some(people) = people.next_permutation() {
         let mut delta = feels[&(people[0], people[people.len() - 1])];
@@ -37,6 +50,5 @@ fn main() {
         }
         max = cmp::max(max, delta);
     }
-
-    println!("{}", max);
+    max
 }
